55b14450371ba8747d14d43d2089f118eb926eb3
Laura <milanbartky@gmail.com>
2023-11-01 18:33:19 +0100

    provide default export

--- glm.js
+++ glm.js
@@ -2,18 +2,20 @@ import { remove_strings, restore_strings } from "./strings.js";
 import { replace_glm } from "./transpile.js";
 import { wrap_parse } from "./parse.js";
 
-export function transpile(pCode) {
-	const { result, strings } = remove_strings(pCode);
+export default class LuaGLMParse {
+	static transpile(pCode) {
+		const { result, strings } = remove_strings(pCode);
 
-	pCode = replace_glm(result);
+		pCode = replace_glm(result);
 
-	return restore_strings(pCode, strings);
-}
+		return restore_strings(pCode, strings);
+	}
 
-export function parse(pCode, pOptions = {}) {
-	const transpiled = transpile(pCode);
+	static parse(pCode, pOptions = {}) {
+		const transpiled = transpile(pCode);
 
-	pOptions.luaVersion = "5.3";
+		pOptions.luaVersion = "5.3";
 
-	return wrap_parse(pCode, transpiled, pOptions);
+		return wrap_parse(pCode, transpiled, pOptions);
+	}
 }
